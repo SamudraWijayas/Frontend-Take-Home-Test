5c57aaa75fc3141217f4af2b81a9ffa893c5b468
SamudraWijayaS <swijayasamdoria@gmail.com>
2026-03-14 22:31:17 +0700

    build

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import DashboardLayout from "@/components/layout/DashboardLayout/DashboardLayout";
 import InvoiceList from "@/components/views/Invoices/InvoiceList";
 import React from "react";
